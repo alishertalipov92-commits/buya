856805bba3f19b2d3aeedeb9927751a482cce612
Your Name <you@example.com>
2026-09-25 10:33:39 +0600

    размер

---
+++ lesson11 js1/scritp.js
@@ -0,0 +1,19 @@
+let phone = 'Honor' 
+
+phone = 'iphone 16'
+
+const price = 2000
+
+console.log(phone)
+
+//  Camel case
+
+let myFirstCode = 'hello world'
+
+//Pascal case
+
+let NewMassage =  'Привет мир'
+
+// Snake case 
+
+let my_first_lesson = 'Биринчи сабак'
